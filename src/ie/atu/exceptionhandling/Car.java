f6b74f1c963235e2d6f34e78c60e7a03ca95bc7d
Daniel Cregg <danielcregg@users.noreply.github.com>
2022-10-18 10:49:29 +0000

    updates

---
+++ src/ie/atu/exceptionhandling/Car.java
@@ -0,0 +1,23 @@
+package ie.atu.exceptionhandling;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Car {
+    // Instance variables
+
+    // Constructor
+
+    // Getters and Setters
+
+    // Methods
+    public void readInCarDetails() throws FileNotFoundException {
+        // The program does not compile, because we use FileReader() throws a checked exception, FileNotFoundException.
+        try {
+            FileReader file = new FileReader("C:\\test\\a.txt");
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        }
+    } // End readInCarDetails method
+
+} // End Class
