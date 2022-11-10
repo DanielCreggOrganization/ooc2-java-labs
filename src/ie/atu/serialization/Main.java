7829b4d6799ae0088f0f1d606c9c678758370f11
xman <xman@lab000000.hwvx02nq5ideppl1mypkwmxdqc.fx.internal.cloudapp.net>
2022-11-10 14:03:43 +0000

    update

--- src/ie/atu/serialization/Main.java
+++ src/ie/atu/serialization/Main.java
@@ -1,7 +1,9 @@
 package ie.atu.serialization;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
@@ -10,12 +12,17 @@ public class Main implements Serializable {
 	public static void main(String[] args) {
 		// Create Person object
 		Person personObject1 = new Person("John", 25);
+		System.out.println("Person object to serialize: " + personObject1.getName() + ", " + personObject1.getAge());
 	
+		// Required for serialization
 		FileOutputStream outputByteFileStream = null;
 		ObjectOutputStream outputByteFileStreamForObjects = null;
-
-		// Serialization
+		// Required for de-serialization
+		FileInputStream inputByteFileStream = null;
+		ObjectInputStream inputByteFileStreamForObjects = null;
+		
 		try {
+			// Serialization
 			// Create a byte stream connected to an output file
 			outputByteFileStream = new FileOutputStream("./resources/personByteFile.ser");
 			// Upgrade the byte stream to accommodate the streaming of Objects
@@ -23,9 +30,20 @@ public class Main implements Serializable {
 			// Commence the serialization of the object
 			outputByteFileStreamForObjects.writeObject(personObject1);
 			System.out.println("Object has been serialized");
-		} catch (IOException ioExc) {
-			System.out.println("IOException was caught");
-			ioExc.printStackTrace();
+			
+			// De-Serialization
+			// Create a byte stream connected to an input file
+			inputByteFileStream = new FileInputStream("./resources/personByteFile.ser");
+			// Upgrade the byte stream to accommodate the streaming of Objects
+			inputByteFileStreamForObjects = new ObjectInputStream(inputByteFileStream);
+			// Commence the de-serialization of the object and store it in a new object
+			Person personObject2 = (Person) inputByteFileStreamForObjects.readObject();
+			System.out.println("Object has been de-serialized");
+			
+			System.out.println("Person object de-serialized: " + personObject2.getName() + ", " + personObject2.getAge());
+		} catch (IOException | ClassNotFoundException e) {
+			System.out.println("Exception was caught");
+			e.printStackTrace();
 		} // End try/catch
 
 	} // End main
