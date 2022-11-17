56bfc3cd17a7181caa07461ad78b3ed1d70615f3
xman <xman@10.0.0.33>
2022-11-17 10:34:02 +0000

    Update

--- src/ie/atu/serialization/Car.java
+++ src/ie/atu/serialization/Car.java
@@ -3,42 +3,21 @@ package ie.atu.serialization;
 import java.io.Serializable;
 
 public class Car implements Serializable {
-	/* 
-	 * The serialVersionUID is a universal version identifier for a Serializable class.
-	 * Deserialization uses this number to ensure that a loaded class corresponds exactly to a serialized object.
+	/*
+	 * The serialVersionUID is a universal version identifier for a Serializable
+	 * class. Deserialization uses this number to ensure that a loaded class
+	 * corresponds exactly to a serialized object.
 	 */
-	private static final long serialVersionUID = -644012276880766677L;
+	private static final long serialVersionUID = 10L;
+
+	// Instance Variables
 	private transient String owner;
-	private String make;
-	private int age;
+	private int ageOfCar;
 
-	// Constructors
-	public Car(String make, int age) {
-		this.make = make;
-		this.age = age;
-	}
-
-	public Car(String make, int age, String owner) {
-		this.make = make;
-		this.age = age;
+	// Constructor
+	public Car(String owner, int ageOfCar) {
+		this.ageOfCar = ageOfCar;
 		this.setOwner(owner);
-	}
-
-	// Getters and Setters
-	public String getMake() {
-		return make;
-	}
-
-	public void setMake(String make) {
-		this.make = make;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
 	}
 
 	public String getOwner() {
@@ -47,5 +26,19 @@ public class Car implements Serializable {
 
 	public void setOwner(String owner) {
 		this.owner = owner;
+	}
+
+	public int getAgeOfCar() {
+		return ageOfCar;
+	}
+
+	public void setAgeOfCar(int ageOfCar) {
+		this.ageOfCar = ageOfCar;
+	}
+	
+	@Override
+	public String toString(){
+		return "Owner: " + this.getOwner() +", Age of Car: " + this.getAgeOfCar();
+		
 	}
 }
--- src/ie/atu/serialization/Main.java
+++ src/ie/atu/serialization/Main.java
@@ -1,18 +1,21 @@
 package ie.atu.serialization;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		Employee employeeObject1 = new Employee("827384773H", "Collins");
-		System.out.println("PPSN: " + employeeObject1.getPpsn()+ ", Surname: " + employeeObject1.getSurname() + ", Company: " + Employee.COMPANY_NAME);
+		System.out.println("PPSN: " + employeeObject1.getPpsn() + ", Surname: " + employeeObject1.getSurname()
+				+ ", Company: " + Employee.COMPANY_NAME);
 
 		// Declare required streams
 		FileOutputStream outputByteFileStream = null;
@@ -32,7 +35,7 @@ public class Main {
 		} catch (IOException IOExc) {
 			IOExc.printStackTrace();
 		}
-		
+
 		// DE-SERIALIZATION
 		try {
 			// Create a file input stream
@@ -41,9 +44,55 @@ public class Main {
 			inputByteFileStreamForObjects = new ObjectInputStream(inputByteFileStream);
 			// De-Serialize object
 			Employee employeeObject2 = (Employee) inputByteFileStreamForObjects.readObject();
-			System.out.println( employeeObject2.getPpsn() + " " + employeeObject2.getSurname() + " " + Employee.COMPANY_NAME);
+			System.out.println(
+					employeeObject2.getPpsn() + " " + employeeObject2.getSurname() + " " + Employee.COMPANY_NAME);
 			System.out.println("Object has been deserialized");
-			System.out.println("PPSN: " + employeeObject1.getPpsn()+ ", Surname: " + employeeObject1.getSurname() + ", Company: " + Employee.COMPANY_NAME);
+			System.out.println("PPSN: " + employeeObject1.getPpsn() + ", Surname: " + employeeObject1.getSurname()
+					+ ", Company: " + Employee.COMPANY_NAME);
+		} catch (IOException IOExc) {
+			IOExc.printStackTrace();
+		} catch (ClassNotFoundException CNFExc) {
+			CNFExc.printStackTrace();
+		}
+
+		// PART 2: Serialize and De-Serialize a list of object. A list itself is an
+		// object.
+
+		// Create list of Cars
+		List<Car> carList1 = new ArrayList<>();
+
+		// Create 2 Car objects to be serialized
+		Car carObject1 = new Car("John F. Kennedy", 3);
+		Car carObject2 = new Car("Micheal D. Higgins", 5);
+
+		// Add car objects to list
+		carList1.add(carObject1);
+		carList1.add(carObject2);
+		System.out.println(carList1);
+		
+		// Serialize List of Cars
+		try {
+			// Set up object stream to file
+			outputByteFileStream = new FileOutputStream("./resources/cars.ser");
+			outputByteFileStreamForObjects = new ObjectOutputStream(outputByteFileStream);
+			// Serialize Car object and stream to file for storage
+			// objectOutStream.writeObject(carObject1);
+			// objectOutStream.writeObject(carObject2);
+			outputByteFileStreamForObjects.writeObject(carList1);
+		} catch (IOException IOExc) {
+			IOExc.printStackTrace();
+		}
+		
+		// De-Serialize List of Cars
+		try {
+			// Create a file input stream
+			inputByteFileStream = new FileInputStream("./resources/cars.ser");
+			// Upgrade file input stream to object input stream
+			inputByteFileStreamForObjects = new ObjectInputStream(inputByteFileStream);
+			// De-Serialize object
+			List<Car> carList2 = (ArrayList<Car>) inputByteFileStreamForObjects.readObject();
+			System.out.println("Car list object has been deserialized");
+			System.out.println(carList2.toString());
 		} catch (IOException IOExc) {
 			IOExc.printStackTrace();
 		} catch (ClassNotFoundException CNFExc) {
