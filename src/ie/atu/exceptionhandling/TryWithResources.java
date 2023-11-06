92faedbd2d0215c40ce9fe61b1c134d68df0ab1e
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-11-06 11:29:03 +0000

    Add try with reoursces example

---
+++ src/ie/atu/exceptionhandling/TryWithResources.java
@@ -0,0 +1,20 @@
+package ie.atu.exceptionhandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources {
+    // Demonstrating the try-with-resources block.
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("file.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
+        }
+
+    }
+}
