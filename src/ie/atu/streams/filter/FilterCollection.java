10169388d064eade05a3c8b3d78b3f5ae96f2cf4
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-11-09 12:36:28 +0000

    Refactor FilterCollection.java to use streams

--- src/ie/atu/streams/filter/FilterCollection.java
+++ src/ie/atu/streams/filter/FilterCollection.java
@@ -14,21 +14,23 @@ public class FilterCollection {
         numbers.add(4);
         numbers.add(5);
 
-        // Filter the ArrayList using a stream to get only the even numbers. Use the filter() method to 
-        // filter the streamand the collect() method to collect the filtered stream into a list.
+        // Filter the ArrayList using a stream to get only the even numbers. Use the filter()
+        // method to filter the streamand the collect() method to collect the filtered stream into a list.
         List<Integer> evenNumbers = numbers.stream()
-            .filter(n -> n % 2 == 0) // Filter the numbers to get only the even numbers
-            .collect(Collectors.toList()); // Collect the even numbers into a list
+                .filter(n -> n % 2 == 0) // Filter the numbers to get only the even numbers
+                .collect(Collectors.toList()); // Collect the even numbers into a list
 
         // Print the even numbers
         System.out.println(evenNumbers);
 
-        // Use the stream to add one to each number in the list. Use the map() method to map each number
-        // to a new number and the collect() method to collect the mapped stream into a list.
+        // Use the stream to add one to each number in the list. Use the map() method to
+        // map each number
+        // to a new number and the collect() method to collect the mapped stream into a
+        // list.
         List<Integer> incrementedNumbers = numbers.stream()
-            .map(n -> n + 1) // Map each number to a new number
-            .collect(Collectors.toList()); // Collect the mapped stream into a list
-        
+                .map(n -> n + 1) // Map each number to a new number
+                .collect(Collectors.toList()); // Collect the mapped stream into a list
+
         // Print the incremented numbers
         System.out.println(incrementedNumbers);
     }
