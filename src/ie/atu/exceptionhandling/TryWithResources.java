89231f2673e9c921b06efac0c31f330857718a84
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-11-09 11:29:17 +0000

    Fix typos and update file path in TryWithResources.java

--- src/ie/atu/exceptionhandling/TryWithResources.java
+++ src/ie/atu/exceptionhandling/TryWithResources.java
@@ -1,20 +1,25 @@
+
+/**
+ * Demonstrates the use of try-with-resources block to read a file using BufferedReader.
+ * The try-with-resources block automatically closes the resources declared in the try block.
+ */
 package ie.atu.exceptionhandling;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.BufferedReader; // Userd to read a file line by line.
+import java.io.FileReader; // Used to read a file.
+import java.io.IOException; // Used to handle IO exceptions.
 
 public class TryWithResources {
-    // Demonstrating the try-with-resources block.
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader("file.txt"))) {
-            String line;
+        // Try-with-resources block to read a file. The resources declared in the try block
+        // will be closed automatically.
+        try (BufferedReader br = new BufferedReader(new FileReader("./resources/inputHamlet.txt"))) {
+            String line; // Used to store the line read from the file.
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
         } catch (IOException e) {
             System.out.println("An error occurred while reading the file: " + e.getMessage());
         }
-
     }
 }
