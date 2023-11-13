2ea4af02152237aba4beb2367ca4a5660a8992ea
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-11-13 12:24:46 +0000

    Refactor Car class to implement Serializable interface

--- src/ie/atu/serialization/listserialization/Car.java
+++ src/ie/atu/serialization/listserialization/Car.java
@@ -1,71 +1,45 @@
 package ie.atu.serialization.listserialization;
 
-import java.io.Serializable;
-import java.util.Objects;
+import java.io.Serializable; // Required to implement Serializable
 
-/**
- * Represents a car with an owner and age.
- */
 public class Car implements Serializable {
 	// The serialVersionUID is a universal version identifier for a Serializable class.
 	// Deserialization uses this number to ensure that a loaded class corresponds exactly to a serialized object.
 	private static final long serialVersionUID = 10L;
 
 	// Instance Variables
-	// The owner of the car. This field is transient, so it will not be serialized.
-	private transient String owner;
-	// The age of the car.
-	private int ageOfCar;
+	private transient String owner; // This field is transient, so it will not be serialized.
+	private int ageOfCar; // The age of the car.
 
-	/**
-	 * Constructs a new Car with the specified owner and age.
-	 *
-	 * @param owner the owner of the car
-	 * @param ageOfCar the age of the car
-	 */
+	// Constructs a new Car with the specified owner and age.
 	public Car(String owner, int ageOfCar) {
-		this.setOwner(owner);
-		this.setAgeOfCar(ageOfCar);
+		this.owner = owner;
+		this.ageOfCar = ageOfCar;
 	}
 
-	/**
-	 * Returns the owner of the car.
-	 *
-	 * @return the owner of the car
-	 */
+	// Returns the owner of the car.
 	public String getOwner() {
 		return owner;
 	}
 
-	/**
-	 * Sets the owner of the car.
-	 *
-	 * @param owner the new owner of the car
-	 */
+	// Sets the owner of the car.
 	public void setOwner(String owner) {
 		this.owner = owner;
 	}
 
-	/**
-	 * Returns the age of the car.
-	 *
-	 * @return the age of the car
-	 */
+	// Returns the age of the car.
 	public int getAgeOfCar() {
 		return ageOfCar;
 	}
 
-	/**
-	 * Sets the age of the car.
-	 *
-	 * @param ageOfCar the new age of the car
-	 */
+	// Sets the age of the car.
 	public void setAgeOfCar(int ageOfCar) {
 		this.ageOfCar = ageOfCar;
 	}
 
+	// Override the toString() method to returns a string representation of this Car.
 	@Override
 	public String toString() {
-		return "Owner: " + Objects.toString(this.getOwner()) + ", Age of Car: " + this.getAgeOfCar();
+		return "Owner: " + this.getOwner() + ", Age of Car: " + this.getAgeOfCar();
 	}
 }
