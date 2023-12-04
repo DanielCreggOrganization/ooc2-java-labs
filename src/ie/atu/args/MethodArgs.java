b55f17c8cf38cef4e69b0f50f40c534986ac4fcf
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-12-04 14:56:50 +0000

    Update add method arguments

--- src/ie/atu/args/MethodArgs.java
+++ src/ie/atu/args/MethodArgs.java
@@ -5,14 +5,14 @@ public class MethodArgs {
 	public static void main(String[] args) {
 		
 		// Call the add method. Pass two integers as arguments to this method.
-		add(5, 6); 
+		add(4, 11); 
 				
 	} // End main
 
 	// add method with two input parameters, one integer named x and one named y.
 	static void add(int x, int y) {
-		System.out.println("Argument 1: " + x);
-		System.out.println("Argument 2: " + y);
+		System.out.println("Argument 1 = " + x);
+		System.out.println("Argument 2 = " + y);
 		System.out.println("Argument 1 + Argument 2 = " + (x + y));
 	
 	} // End add method
